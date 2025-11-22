c9b365d041f420827f4b26013b809b9d829725cd
yujin-fe <hyujin1224@gmail.com>
2025-11-22 00:27:49 +0000

    [✨Feature/60] DropdownMenuCheckItem 컴포넌트 구현 (#70)

--- src/components/dashboard/dropdown/DropdownMenuItem.tsx
+++ src/components/dashboard/dropdown/DropdownMenuItem.tsx
@@ -1,6 +1,6 @@
 export default function DropdownMenuItem({ children }: { children: React.ReactNode }) {
   return (
-    <li className='cursor-pointer rounded-sm px-4 py-1 text-center font-md-regular text-gray-700 select-none hover:bg-violet-500-8 hover:text-violet-500'>
+    <li className='dropdown-menu-item-base py-1 text-center font-md-regular hover:bg-violet-500-8 hover:text-violet-500'>
       {children}
     </li>
   );
